d212e6df55cfa6c22c1eb915d986f4a13e8ab2e6
Eliot Slevin <Eliot.slevin@gmail.com>
2015-05-18 23:51:15 +1200

    Better page transitions
    No dom rebuilding!

---
+++ js/pagetransitions.js
@@ -0,0 +1,147 @@
+var PageTransitions = (function() {
+
+	var $main = $( '#pt-main' ),
+		$pages = $main.children( 'div.pt-page' ),
+		$iterate = $( '#iterateEffects' ),
+		animcursor = 1,
+		pagesCount = $pages.length,
+		current = 0,
+		isAnimating = false,
+		endCurrPage = false,
+		endNextPage = false,
+		animEndEventNames = {
+			'WebkitAnimation' : 'webkitAnimationEnd',
+			'OAnimation' : 'oAnimationEnd',
+			'msAnimation' : 'MSAnimationEnd',
+			'animation' : 'animationend'
+		},
+		// animation end event name
+		animEndEventName = animEndEventNames[ Modernizr.prefixed( 'animation' ) ],
+		// support css animations
+		support = Modernizr.cssanimations;
+	
+	function init() {
+
+
+		$( "#forward" ).click(function() {
+			var options = {animation: 1};
+			nextPage(options);
+		});
+
+		$( "#backwards" ).click(function() {
+			var options = {animation: 2};
+			nextPage(options);
+		});
+
+
+		$pages.each( function() {
+			var $page = $( this );
+			$page.data( 'originalClassList', $page.attr( 'class' ) );
+		} );
+
+		$pages.eq( current ).addClass( 'pt-page-current' );
+
+
+		palette.init();
+        execution_pane.init();
+
+        var canvas = paper.setup(document.getElementById("paper_canvas"));
+        init_drawing(canvas);
+
+        console.log($(".play_button"));
+
+        $(".play_button").click(function(){
+          console.log("Running Execution");
+          execution_pane.run(canvas);
+        });
+
+
+	}
+
+	function nextPage(options) {
+		var animation = (options.animation) ? options.animation : options;
+
+		if( isAnimating ) {
+			return false;
+		}
+
+		isAnimating = true;
+		
+		var $currPage = $pages.eq( current );
+
+		if(options.showPage){
+			if( options.showPage < pagesCount - 1 ) {
+				current = options.showPage;
+			}
+			else {
+				current = 0;
+			}
+		}
+		else{
+			if( current < pagesCount - 1 ) {
+				++current;
+			}
+			else {
+				current = 0;
+			}
+		}
+
+		var $nextPage = $pages.eq( current ).addClass( 'pt-page-current' ),
+			outClass = '', inClass = '';
+
+		switch( animation ) {
+			case 1:
+				outClass = 'pt-page-fade';
+				inClass = 'pt-page-moveFromLeft pt-page-ontop';
+				break;
+
+			case 2:
+				outClass = 'pt-page-reverseMoveFromLeft pt-page-ontop';
+				inClass = 'pt-page-reverseFade';
+				break;
+			
+
+		}
+
+		$currPage.addClass( outClass ).on( animEndEventName, function() {
+			$currPage.off( animEndEventName );
+			endCurrPage = true;
+			if( endNextPage ) {
+				onEndAnimation( $currPage, $nextPage );
+			}
+		} );
+
+		$nextPage.addClass( inClass ).on( animEndEventName, function() {
+			$nextPage.off( animEndEventName );
+			endNextPage = true;
+			if( endCurrPage ) {
+				onEndAnimation( $currPage, $nextPage );
+			}
+		} );
+
+		if( !support ) {
+			onEndAnimation( $currPage, $nextPage );
+		}
+
+	}
+
+	function onEndAnimation( $outpage, $inpage ) {
+		endCurrPage = false;
+		endNextPage = false;
+		resetPage( $outpage, $inpage );
+		isAnimating = false;
+	}
+
+	function resetPage( $outpage, $inpage ) {
+		$outpage.attr( 'class', $outpage.data( 'originalClassList' ) );
+		$inpage.attr( 'class', $inpage.data( 'originalClassList' ) + ' pt-page-current' );
+	}
+
+	init();
+
+	return { 
+		init : init,
+		nextPage : nextPage,
+	};
+
+})();
