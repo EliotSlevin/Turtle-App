9b15e28d2c2edc0de80936e77dfabaaa6ea0c6eb
gregoswald <oswald.gm@gmail.com>
2015-06-19 02:28:02 +0000

    updated load_user_sketch to handle loading user sketches

--- js/browser.js
+++ js/browser.js
@@ -16,12 +16,12 @@ function renderSketches(data, online){
   });
 }
 
-function renderUserSketches(data, online){
+function renderUserSketches(userName,data, online){
   var source   = $("#sketch-browser-template").html();
   var template = Handlebars.compile(source);
   var html = template(data);
   $(".sketch-browser").html(html);
-  $(".sketch-browser").prepend("<div></div><div class=\"user-title-bar\"><h2>Usernames's Sketches</h2><i class=\"fa fa-arrow-left\"></i></div><div></div>");
+  $(".sketch-browser").prepend("<div></div><div class=\"user-title-bar\"><h2>"+userName+"'s Sketches</h2><i class=\"fa fa-arrow-left\"></i></div><div></div>");
   $(".open-sketch").click(function(){
     if(online)serverside.load_sketch($(this).parent().attr("data-id"));
     else storage.load_local_sketch($(this).parent().attr("data-id"));
@@ -80,17 +80,9 @@ browser.load_local_sketches = function(offset){
 }
 
 
-browser.load_userpage = function(offset){
-  if(browser.online.last_fetched_sketches){
-    var diff = Math.floor(Date.now() / 1000) - browser.online.last_fetch_time;
-    if(diff < 300){
-      //If we have fetched in the last 5 minutes, use the cached result
-      renderSketches(browser.online.last_fetched_sketches, true);
-      return;
-    }
-  }
-
-  serverside.load_popular_sketches(9, offset, "popular", function(data){
+browser.load_userpage = function(userName,offset){
+ 
+ serverside.load_sketches_by_user(userName,offset,function(data){
     var dataContext = {sketches:[]};
     for(var i = 0;i < data.sketches.length;i ++){
       var sketch = data.sketches[i];
@@ -98,14 +90,10 @@ browser.load_userpage = function(offset){
         name: sketch.name,
         preview: sketch.preview,
         sketchid: sketch.sketchid,
-        username: sketch.username
+	username: "" 
       });
     }
-
-    browser.online.last_fetch_time = Math.floor(Date.now() / 1000);
-    browser.online.last_fetched_sketches = dataContext;
-
-    renderUserSketches(dataContext, true);
+	 renderUserSketches(userName,dataContext, true);
   });
 
 }
